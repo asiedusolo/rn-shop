1ecebdc603d13dfbae8e19d0e6c1d21954779e04
asiedusolo <asiedusolo.sa@gmail.com>
2024-11-04 08:26:18 +0000

    feat: some basic pages

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my first mobile app. Another change hello</Text>
+      <Text>This is my first mobile app. Another</Text>
       <StatusBar style="auto" />
     </View>
   );
